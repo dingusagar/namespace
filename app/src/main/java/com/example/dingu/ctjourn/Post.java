17528db2e775a917e1484a02940ce453f9aa5526
dingusagar <dingusagar@gmail.com>
2017-10-07 03:58:01 +0530

    syncing projects works

--- app/src/main/java/com/example/dingu/ctjourn/Post.java
+++ app/src/main/java/com/example/dingu/ctjourn/Post.java
@@ -1,11 +1,7 @@
 package com.example.dingu.ctjourn;
 
 
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.support.v4.app.ActivityCompat;
+
 
 import java.util.HashMap;
 
@@ -33,7 +29,7 @@ public class Post {
     private String Title;
     private String video;
     private String Desc;
-    private Double latitude, longitude;
+
 
 
     public String getTitle() {
@@ -79,22 +75,6 @@ public class Post {
         this.username = username;
     }
 
-    public void setGPSCoordinates(Context context) {
-        LocationManager locationManager;
-        LocationListener locationListener;
-        locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) this);
-    }
 
 
 }
